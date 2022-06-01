c7b1aee80fe6281186f231886a750f047ef0d6ce
n17r053v3n <n17r053v3n@protonmail.com>
2022-06-01 12:28:24 +0200

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "conainer_id" {
+    description = "ID of the Docker container"
+    value = docker_container.nginx.id  
+}
+
+output "image_id" {
+    description = "ID of the Docker image"
+    value = docker_image.nginx.id  
+}
